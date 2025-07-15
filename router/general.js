9f7a8e585ff34ef1edd017229b2b990e00aba9e9
rena <renlopez08@gmail.com>
2025-07-15 11:54:20 -0300

    Implementadas tareas 1 a 13 del proyecto final

--- router/general.js
+++ router/general.js
@@ -3,9 +3,8 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-const axios = require('axios');
 
-
+// Registro de usuario
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -21,28 +20,24 @@ public_users.post("/register", (req,res) => {
   return res.status(201).json({message: "User registered successfully"});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  const books = require("./booksdb.js").books;
-
+// Obtener todos los libros
 public_users.get('/', function (req, res){
-  res.send(JSON.stringify(books, null, 4));
-})
+  res.json(books);
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+// Obtener libro por ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
 
   if (books[isbn]) {
-    return res.send(books[isbn])
+    return res.json(books[isbn]);
   } else {
     return res.status(404).json({message: "Book not found"});
   }
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+});
+
+// Obtener libros por autor
+public_users.get('/author/:author', function (req, res) {
   const author = req.params.author.toLowerCase();
   let results = [];
 
@@ -51,16 +46,16 @@ public_users.get('/author/:author',function (req, res) {
       results.push(book);
     }
   });
-  
+
   if (results.length > 0) {
-    return res.send(results);
+    return res.json(results);
   } else {
     return res.status(404).json({message: "No books found for this author"});
   }
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+// Obtener libros por título
+public_users.get('/title/:title', function (req, res) {
   const title = req.params.title.toLowerCase();
   let results = [];
 
@@ -71,18 +66,18 @@ public_users.get('/title/:title',function (req, res) {
   });
 
   if (results.length > 0) {
-    return res.send(results);
+    return res.json(results);
   } else {
     return res.status(404).json({message: "No books found with this title"});
   }
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+// Obtener reseñas del libro por ISBN
+public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn;
 
   if (books[isbn]) {
-    return res.send(books[isbn].reviews);
+    return res.json(books[isbn].reviews);
   } else {
     return res.status(404).json({message: "Book not found"});
   }
